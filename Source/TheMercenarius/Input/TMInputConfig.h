98c6e3d6c30d4b845ef9451313ef72b9800fd554
LAPTOP-TANPRLPA\huil3 <huil33@naver.com>
2026-06-30 16:41:40 +0900

    [이후일] IC/IMC/IA_Move 추가

---
+++ Source/TheMercenarius/Input/TMInputConfig.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "TMInputConfig.generated.h"
+
+class UInputAction;
+/**
+ * 
+ */
+UCLASS()
+class THEMERCENARIUS_API UTMInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UInputAction> Move;
+};
