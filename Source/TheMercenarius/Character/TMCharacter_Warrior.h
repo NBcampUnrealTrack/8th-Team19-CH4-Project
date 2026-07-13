d7465e5b66a55bfa1db944205155d3af4baa91b5
DESKTOP-TKS9666\1 <namyun2000@gmail.com>
2026-07-13 20:22:22 +0900

    [김남윤][캐릭터]피격 판정, 스킬 범위

--- Source/TheMercenarius/Character/TMCharacter_Warrior.h
+++ Source/TheMercenarius/Character/TMCharacter_Warrior.h
@@ -20,6 +20,9 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	UFUNCTION(BlueprintCallable, Category = "Skill")
+	void ExecuteSkillQImpact();
 
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PlayerComponent", meta = (AllowPrivateAccess = "true"))
@@ -52,6 +55,18 @@ private:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Animation", meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UAnimMontage> SkillRMontage;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Skill|Q", meta = (AllowPrivateAccess = "true"))
+	float SkillQRadius; // 감지할 구체 반지름 (기본값 추천: 300.f)
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Skill|Q", meta = (AllowPrivateAccess = "true"))
+	float SkillQDamage; // 대지가르기 피해량
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Skill|Q", meta = (AllowPrivateAccess = "true"))
+	float SkillQSlowModifier; // 감속할 속도 비율 (예: 0.4f면 속도 40% 감소)
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Skill|Q", meta = (AllowPrivateAccess = "true"))
+	float SkillQSlowDuration; // 슬로우 지속 시간
+
  
 	/* ---------------------------------------------------------
 	 * 스킬 실행 함수
@@ -60,4 +75,6 @@ private:
 	void InputSkillW(const FInputActionValue& Value);
 	void InputSkillE(const FInputActionValue& Value);
 	void InputSkillR(const FInputActionValue& Value);
+	// 실제 충돌 판정을 처리하는 내부 로직 함수
+	void ProcessSphereOverlap();
 }; 
